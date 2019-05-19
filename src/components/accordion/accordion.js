f7b0caaad37e5992ab89f7bdb9b65eb3c5c0b076
anjalyyy1 <anjalyyy1@gmail.com>
2019-05-19 17:32:52 +0530

    Accordion component added

---
+++ src/components/accordion/accordion.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+class Accordion extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            
+        );
+    }
+}
+ 
+export default Accordion;

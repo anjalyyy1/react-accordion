37f16f548874215760ab7327517a6bc51b64deb7
anjalyyy1 <46123557+anjalyyy1@users.noreply.github.com>
2019-05-19 18:15:11 +0530

    remote heading added

--- src/components/accordion/accordion.js
+++ src/components/accordion/accordion.js
@@ -6,6 +6,7 @@ class Accordion extends Component {
 	render() { 
 		return ( 
 				<div className="accordion">
+					<h1>my new heading</h1>
 					<AccordionItem heading="Doggie" description="Dog behavior is the internally coordinated responses (actions or inactions) of the domestic dog (individuals or groups) to internal and/or external stimuli"/>
 					<AccordionItem heading="Cat" description="The cat (Felis catus) is a small carnivorous mammal.[1] It is the only domesticated species in the family Felidae and often referred to as the domestic cat to distinguish it from wild members of the family"/>
 					<AccordionItem heading="Sparrow" description="Sparrows are a family of small passerine birds. They are also known as true sparrows, or Old World sparrows, names also used for a particular genus of the family"/>
